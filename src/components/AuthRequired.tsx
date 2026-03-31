887e569d87f1556ee38c8d315ec2af1fc7373b53
Eug4n4 <Eug4n4@users.noreply.github.com>
2026-03-31 17:31:36 +0300

    fix: auth

--- src/components/AuthRequired.tsx
+++ src/components/AuthRequired.tsx
@@ -1,16 +1,43 @@
+import { api } from "@/lib/api";
+import { logout, setLoading } from "@/state/auth/auth.slice";
 import type { IRootState } from "@/state/store";
-import type { PropsWithChildren } from "react";
-import { useSelector } from "react-redux";
+import Cookies from "js-cookie";
+import { useEffect, type PropsWithChildren } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const AuthRequired: React.FC<PropsWithChildren> = ({ children }) => {
+  const dispatch = useDispatch();
   const { isAuthenticated, isLoading } = useSelector((state: IRootState) => state.auth);
+
+  const token = Cookies.get("access");
+
+  useEffect(() => {
+    if (isAuthenticated && !token) {
+      dispatch(setLoading(true));
+
+      api
+        .post("accounts/refresh")
+        .catch(() => dispatch(logout()))
+        .finally(() => {
+          dispatch(setLoading(false));
+        });
+    }
+  }, [isAuthenticated, token, dispatch]);
 
   if (isLoading) {
     return null;
   }
 
-  return isAuthenticated ? children : <Navigate to={"/auth"} replace />;
+  if (!isAuthenticated) {
+    return <Navigate to="/auth" replace />;
+  }
+
+  if (!token) {
+    return null;
+  }
+
+  return children;
 };
 
 export default AuthRequired;
